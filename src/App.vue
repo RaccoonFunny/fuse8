c6ccd9b8157070adafb0b348247e7ea457c89033
RaccoonFunny <koltashov.andrey@gmail.com>
2021-07-01 12:51:05 +0300

    Git init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,151 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Our Latest Developments</h1>
+    <label>
+      <span>Filter</span>
+      <input type="text" v-model="filter">
+    </label>
+    <div class="houses">
+      <house-card v-for="house in houses[page]" :house="house" :key="house.id"/>
+    </div>
+    <button class="see-more">
+      See more
+      <svg width="7" height="17" viewBox="0 0 7 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 1L5.043 8.41667L1 15.8333" stroke="#363636" stroke-width="2" stroke-linecap="round"/>
+      </svg>
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HouseCard from "@/components/HouseCard";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {HouseCard},
+  data: function () {
+    return {
+      filter: "",
+      houses: [],
+      page: 0,
+      apiData: [],
+      scrollWidth: 0,
+      blockInScreen: 1,
+    }
+  },
+  watch: {
+    filter: function () {
+      let filter = [];
+      this.houses = [];
+      for (let i = 0; i < this.apiData.length; i++) {
+        if (this.apiData[i].title.toLowerCase().indexOf(this.filter.toLowerCase()) !== -1){
+          filter.push(this.apiData[i]);
+        }
+      }
+      for (let i = 0; i < filter.length; i += this.blockInScreen) {
+        this.houses.push(filter.slice(i, i + this.blockInScreen));
+      }
+    }
+  },
+  beforeMount() {
+    let requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    //узнаём примерную ширину доступного пространства без учёта полосы прокрутки браузера
+    this.scrollWidth = Math.max(
+        document.body.scrollWidth, document.documentElement.scrollWidth,
+        document.body.scrollWidth, document.documentElement.scrollWidth,
+        document.body.scrollWidth, document.documentElement.scrollWidth
+    );
+
+    this.blockInScreen = 2 * Math.floor(this.scrollWidth / 403);
+
+    fetch("https://603e38c548171b0017b2ecf7.mockapi.io/homes", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+          result = JSON.parse(result);
+          this.apiData = result;
+          for (let i = 0; i < result.length; i += this.blockInScreen) {
+            this.houses.push(result.slice(i, i + this.blockInScreen));
+          }
+        })
+        .catch(error => console.log('error', error));
+  },
+  mounted() {
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "./variable.scss";
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Open Sans", sans-serif;
+  font-style: normal;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 50px;
+
+  h1 {
+    margin: 36px 0 43px 0;
+    font-weight: bold;
+    font-size: 36px;
+    line-height: 49px;
+    text-align: center;
+    color: #45852D;
+  }
+
+  label {
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -0.3px;
+    color: $black;
+
+    span {
+      margin-right: 15px;
+    }
+
+    input {
+      border: 1px solid $white;
+      box-sizing: border-box;
+      border-radius: 25px;
+      width: 418px;
+      padding: 16px;
+      margin-bottom: 45px;
+    }
+  }
+
+  .houses {
+    display: grid;
+    grid-gap: 38px 22px;
+    grid-template-columns: repeat(auto-fill, 381px);
+  }
+
+  .see-more {
+    margin: 60px auto;
+    display: flex;
+    padding: 12px 33px 14px 33px;
+    align-items: center;
+    gap: 10px;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -0.3px;
+    color: $black;
+    border: 1px solid $white;
+    border-radius: 25px;
+    background: white;
+    cursor: pointer;
+  }
+
 }
 </style>
---
+++ src/components/HouseCard.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="card">
+    <img src="https://lk.cottage.ru/storage/articles/260/260359/5fccbca8e1468b36c84fc0b39caf219f.jpg?v=1602765804" alt="">
+    <div class="type">
+      <span v-if="house.type === 'IndependentLiving'" class="independent-living">
+        Independent living
+      </span>
+      <span v-if="house.type !== 'IndependentLiving'" class="restaurant-available">
+        Restaurant & Support available
+      </span>
+    </div>
+    <h2 class="title">
+      {{ house.title }}
+    </h2>
+    <p class="address">
+      {{ house.address }}
+    </p>
+    <p class="price">
+      New Properties for Sale from
+      <strong>
+        £{{ house.price }}
+      </strong>
+    </p>
+    <p class="ownership">
+      <small>
+        Shared Ownership Available
+      </small>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HouseCard",
+  props: {
+    house: Object,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../variable.scss";
+
+.card {
+  width: 379px;
+  height: 381px;
+  background: #f8f8f8;
+  border: 1px solid $white;
+
+  img {
+    width: 379px;
+    height: 233px;
+  }
+
+  .title {
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 27px;
+    color: $black;
+    margin: 20px 0 2px 20px;
+  }
+
+  .address {
+    margin: 0 0 15px 20px ;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 22px;
+    color: $black;
+  }
+
+  .price {
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 22px;
+    color: $black;
+    margin: 0 0 7px 20px;
+  }
+
+  .ownership {
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 19px;
+    color: $black;
+    margin-left: 20px;
+  }
+}
+
+.type {
+  text-align: end;
+  margin: -32px 0 0 0;
+}
+
+.independent-living {
+  background: #006F79;
+  padding: 8px 8px 5px 8px;
+  color: #fff;
+}
+
+.restaurant-available {
+  background: #EC6608;
+  padding: 8px 8px 5px 8px;
+  color: #fff;
+}
+
+</style>
